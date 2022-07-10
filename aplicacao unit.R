c94b90188c6d8b761001f13130a4ea073adbe19e
Andersonof30 <80104831+Andersonof30@users.noreply.github.com>
2022-07-09 22:03:49 -0300

    Update aplicacao unit.R

--- aplicacao unit.R
+++ aplicacao unit.R
@@ -1,4 +1,4 @@
-source("bay fun chart.R")
+source("bayesian function chart.R")
 s.min = read.csv("https://raw.githubusercontent.com/Andersonof30/chart/main/serie%20minima%20atualizada%20(n%20200).csv", sep = " ")
 plot.control(s.min$minima, type = 'unit', fase1 = .8, xlab = '', ylab = 'Umidade Relativa do Ar', 
              tit = ' ', size =  20, iter = 1000)
